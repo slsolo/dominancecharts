cfc163bc2ec0877d00995fcc96d415eb71b48f25
Rob Spencer <rob@robspencer.me.uk>
2022-02-15 22:49:41 +0000

    initial

---
+++ go.mod
@@ -0,0 +1,37 @@
+module github.com/slsolo/dominancecharsts
+
+go 1.17
+
+require google.golang.org/api v0.65.0
+
+require (
+	cloud.google.com/go/compute v1.0.0 // indirect
+	github.com/gin-contrib/cors v1.3.1 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-contrib/static v0.0.1 // indirect
+	github.com/gin-gonic/gin v1.7.7 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/ugorji/go/codec v1.2.6 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/net v0.0.0-20220114011407-0dd24b26b47d // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220114231437-d2e6a121cae0 // indirect
+	google.golang.org/grpc v1.43.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	r := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://192.168.1.120:3000"}
+	r.Use(cors.New(config))
+	ctx := context.Background()
+
+	srv, err := sheets.NewService(ctx, option.WithAPIKey(os.Getenv("API_KEY")))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+
+	spreadsheetId := "181njrl_PeCETmTjiKM9H4AJP2l_4-e-TPS7bBoWgTPQ"
+	readRange := "Fur!B2:B189"
+	resp, err := srv.Spreadsheets.Get(spreadsheetId).Ranges(readRange).IncludeGridData(true).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+	}
+
+	placed := make(map[string]int)
+	for _, sht := range resp.Sheets {
+		for _, row := range sht.Data {
+			for p, cell := range row.RowData {
+				for _, val := range cell.Values {
+					placed[val.FormattedValue] = p + 1
+				}
+			}
+		}
+	}
+
+	keys := make([]string, len(placed))
+
+	i := 0
+
+	for k := range placed {
+		keys[i] = k
+		i++
+	}
+
+	sort.Strings(keys)
+	r.Use(static.Serve("/", static.LocalFile("./ui/dist", false)))
+	r.GET("/api/furs", func(c *gin.Context) {
+		c.JSON(200, placed)
+	})
+
+	r.GET("/api/furs/names", func(c *gin.Context) {
+		c.JSON(200, keys)
+	})
+
+	r.GET("/api/furs/:name", func(c *gin.Context) {
+		c.JSON(200, placed[c.Param("name")])
+	})
+
+	r.GET("/api/furs/compare/:first/:second", func(c *gin.Context) {
+		if placed[c.Param("first")] == placed[c.Param("second")] {
+			c.String(400, "Both furs are the same. Please change your selection and try again.")
+		} else if placed[c.Param("first")] < placed[c.Param("second")] {
+			c.String(200, fmt.Sprintf("%s is dominant to %s", c.Param("first"), c.Param("second")))
+		} else {
+			c.String(200, fmt.Sprintf("%s is recessive to %s", c.Param("first"), c.Param("second")))
+		}
+	})
+
+	r.Run()
+}
