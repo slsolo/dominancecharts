432bd27d03866591b0bf9071df5a3c5cc52b77df
Spencer Robert Dr (Elec Electronic Eng) <robert.spencer@surrey.ac.uk>
2022-04-09 22:37:55 +0100

    fix(handlers): finish conversion to gorilla mux

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 	"sort"
 	"strconv"
@@ -42,7 +41,8 @@ func (s *application) TraitGetHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if len(values) == 0 {
-		c.String(http.StatusNotFound, fmt.Sprintf("no %ss found", c.Param("trait")))
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "No %ss found.\n", vars["trait"])
 		return
 	}
 
@@ -50,75 +50,93 @@ func (s *application) TraitGetHandler(w http.ResponseWriter, r *http.Request) {
 	for i := 0; i < respLen; i += 2 {
 		placed[values[i]], _ = strconv.Atoi(values[i+1])
 	}
-	c.JSON(http.StatusOK, placed)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(placed)
 }
 
 func (s *application) TraitGetValueHandler(w http.ResponseWriter, r *http.Request) {
-	values, err := s.getData("names", strings.Title(c.Param("trait")), c.Param("name"))
+	vars := mux.Vars(r)
+	values, err := s.getData("names", strings.Title(vars["trait"]), vars["name"])
 	if err != nil {
-		c.String(http.StatusInternalServerError, err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "%s\n", err.Error())
 		return
 	}
 
 	if len(values) == 0 {
-		c.String(http.StatusNotFound, "%s %s not found\n", c.Param("Trait"), c.Param("name"))
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s %s not found.\n", vars["trait"], vars["name"])
 		return
 	}
 	var i int
 	i, err = strconv.Atoi(values[0])
 	if err != nil {
-		c.String(http.StatusInternalServerError, "Value is not an integer")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "value (%s) is not an integer\n", values[0])
 		return
 	}
-	c.JSON(200, i)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(i)
 }
 
 func (s *application) TraitCompareHandler(w http.ResponseWriter, r *http.Request) {
-	trait := strings.Title(c.Param("trait"))
-	first := c.Param("first")
-	second := c.Param("second")
+	vars := mux.Vars(r)
+	trait := strings.Title(vars["trait"])
+	first := vars["first"]
+	second := vars["second"]
 	var fi, si int
 	values, err := s.getData("single", trait, first)
 	if err != nil {
-		c.String(http.StatusInternalServerError, err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "%s\n", err.Error())
 		return
 	}
 
 	if len(values) == 0 {
-		c.String(http.StatusNotFound, "%s %s not found\n", trait, first)
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s %s not found.\n", trait, first)
 		return
 	}
 
 	fi, err = strconv.Atoi(values[0])
 	if err != nil {
-		log.Printf("trying to parse %s\n", values[0])
-		c.String(http.StatusInternalServerError, err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "value (%s) is not an integer\n", values[0])
 		return
 	}
 
 	values, err = s.getData("single", trait, second)
 	if err != nil {
-		c.String(http.StatusInternalServerError, err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "%s\n", err.Error())
 		return
 	}
 
 	if len(values) == 0 {
-		c.String(http.StatusNotFound, "%s %s not found\n", trait, second)
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s %s not found.\n", trait, second)
 		return
 	}
 
 	si, err = strconv.Atoi(values[0])
 	if err != nil {
-		c.String(http.StatusInternalServerError, err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "value (%s) is not an integer\n", values[0])
 		return
 	}
 
 	if first == second {
-		c.String(400, "Both %ss are the same. Please change your selection and try again.", trait)
-	} else if fi < si {
-		c.String(200, fmt.Sprintf("%s is dominant to %s", first, second))
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Both %ss are the same. Please change your selection and try again.", trait)
 	} else {
-		c.String(200, fmt.Sprintf("%s is recessive to %s", first, second))
-	}
+		w.WriteHeader(http.StatusOK)
+		if fi < si {
+			fmt.Fprintf(w, "%s is dominant to %s", first, second)
+		} else {
+			fmt.Fprintf(w, "%s is recessive to %s", first, second)
+		}
 
+	}
 }
